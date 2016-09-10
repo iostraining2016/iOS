c98bde01a6926b036e2e4145f16fb487ffee5f8b
Victor Cabrales <victor.cabrales@gmail.com>
2016-09-10 12:58:52 -0500

    Initial commit

---
+++ Note Editor/Note Editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Note Editor
+//
+//  Created by Victor Cabrales on 9/10/16.
+//  Copyright © 2016 iOS Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
